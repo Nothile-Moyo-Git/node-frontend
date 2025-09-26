2d856fb1e85452e5dd037d30d93fbeffb42a34a1
Nothile Moyo <Nothile1@gmail.com>
2025-09-26 23:23:24 +0100

    Wrote a basic test for editing posts

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Date created: 25/09/2025
+ * Author: Nothile Moyo
+ *
+ * Styling definitions for typescript issues
+ */
+
+declare module "*.scss";
+declare module "*.sass";
+
+declare module "*.module.scss";
+declare module "*.module.sass";
