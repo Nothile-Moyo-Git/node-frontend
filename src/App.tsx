6200be7de639ce096c910d7753bf396591fc57c6
Nothile Moyo <Nothile1@gmail.com>
2025-11-27 22:16:15 +0000

    I've refactored my App component now so the hook is separate. This should make testing far easier

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,42 @@
 /**
  * Date created : 10/02/2024
  * Author : Nothile Moyo
+ *
+ * Description: Main app file, houses the dashboard and also handles validation for the user
  */
 
-import { FC, useState, useEffect, useContext } from "react";
+import { FC, useContext } from "react";
 import { AppContext } from "./context/AppContext";
 import LoadingSpinner from "./components/loader/LoadingSpinner";
-import { BASENAME, checkSessionValidation } from "./util/util";
+import { BASENAME } from "./util/util";
 import "./App.scss";
-import { User } from "./@types";
 import { useNavigate } from "react-router-dom";
 import ErrorModal from "./components/modals/variants/ErrorModal";
+import useUserDetails from "./hooks/useUserDetails";
 
 const App: FC = () => {
   const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [loadingError, setLoadingError] = useState<boolean>(false);
-  const [user, setUser] = useState<User>();
-  const [sessionExpiryDate, setSessionExpiryDate] = useState<string>();
-  const [sessionCreationDate, setSessionCreationDate] = useState<string>();
+  const { isLoading, error, user, sessionCreated, sessionExpires } = useUserDetails();
 
-  // Get the method from the backend to query
   const appContextInstance = useContext(AppContext);
 
-  // Query the backend to see if we're logged in
-  useEffect(() => {
-    // Get user details if the user is authenticated from the backend
-    const getUserDetails = async (userId: string) => {
-      // Perform the fetch request using GraphQL in order to get the user details on the main app page
-      // Note: Please convert your id to an objectId in the backend
-      const response = await fetch(`${appContextInstance?.baseUrl}/graphql/auth`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify({
-          query: `
-            query PostUserDetailsResponse($_id : String!, $token : String!){
-              PostUserDetailsResponse(_id : $_id, token : $token){
-                user {
-                  _id
-                  name
-                  email
-                  password
-                  confirmPassword
-                  status
-                  posts
-                }
-                sessionCreated
-                sessionExpires
-                success
-                }
-              }
-            `,
-          variables: {
-            _id: userId,
-            token: appContextInstance?.token ?? "",
-          },
-        }),
-      });
-
-      // Get the result from the endpoint
-      const {
-        data: {
-          PostUserDetailsResponse: { sessionCreated, sessionExpires, user, success },
-        },
-      } = await response.json();
-      // Set the user details so we can render session data
-      setLoadingError(!success);
-      setUser(user);
-      setSessionExpiryDate(sessionExpires);
-      setSessionCreationDate(sessionCreated);
-    };
-
-    const fetchAuthentication = async () => {
-      try {
-        appContextInstance?.validateAuthentication();
-        console.log("Is user authenticated: ", appContextInstance?.userAuthenticated);
-
-        if (appContextInstance?.userAuthenticated && appContextInstance.userId) {
-          await getUserDetails(appContextInstance.userId);
-        }
-
-        if (appContextInstance?.userAuthenticated && appContextInstance.userId && appContextInstance.token) {
-          await checkSessionValidation(appContextInstance.userId, appContextInstance.token, appContextInstance.baseUrl);
-        }
-      } catch (error) {
-        console.error(error);
-        // We have a response so we're not loading data anymore
-        if (error) {
-          setLoadingError(true);
-        }
-      } finally {
-        setIsLoading(false);
-      }
-
-      if (!appContextInstance?.userAuthenticated) {
-        navigate(`${BASENAME}/login`);
-      }
-    };
-
-    fetchAuthentication();
-  }, [appContextInstance, navigate]);
+  if (!isLoading && !appContextInstance?.userAuthenticated) {
+    navigate(`${BASENAME}/login`);
+  }
 
   return (
-    <div className="app" data-testid="test-id-app-component">
+    <div data-testid="test-id-app-component">
       {isLoading && <LoadingSpinner />}
 
-      {!isLoading && loadingError && <ErrorModal testId="test-id-error-modal" />}
+      {!isLoading && error && <ErrorModal testId="test-id-error-modal" />}
 
-      {!isLoading && appContextInstance?.userAuthenticated && user && (
+      {!isLoading && appContextInstance?.userAuthenticated && user && !error && (
         <div className="app__content">
           <h1 className="app__title">{`Welcome ${user?.name}`}</h1>
           <p className="app__text">{`Current status : ${user?.status}`}</p>
           <p className="app__text">{`Email address : ${user?.email}`}</p>
-          <p className="app__text">{`Session created : ${sessionCreationDate}`}</p>
-          <p className="app__text">{`Session expires : ${sessionExpiryDate}`}</p>
+          <p className="app__text">{`Session created : ${sessionCreated}`}</p>
+          <p className="app__text">{`Session expires : ${sessionExpires}`}</p>
         </div>
       )}
     </div>
--- src/hooks/useUserDetails.ts
+++ src/hooks/useUserDetails.ts
@@ -10,6 +10,7 @@
 import { useEffect, useState, useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import { User } from "../@types";
+import { checkSessionValidation } from "../util/util";
 
 //
 interface UserDetailsInterface {
@@ -20,6 +21,22 @@ interface UserDetailsInterface {
   sessionExpires: string;
 }
 
+/**
+ * @name useUserDetails
+ *
+ * @description Hook which handles getting the user details from the backend. Returns an error in state if false
+ *
+ * @types UserDetailsInterface:
+ * isLoading: boolean
+ * error: boolean
+ * user: User
+ * sessionCreated: string
+ * sessionExpires: string
+ *
+ * @params none
+ * @returns userDetails : UserDetailsInterface
+ *
+ */
 const useUserDetails = () => {
   // Get our context from the backend for our session details we get from local storage
   const context = useContext(AppContext);
@@ -33,18 +50,19 @@ const useUserDetails = () => {
     sessionExpires: "",
   });
 
-  // Get user details if the user is authenticated from the backend
-  const getUserDetails = async () => {
-    // Perform the fetch request using GraphQL in order to get the user details on the main app page
-    // Note: Please convert your id to an objectId in the backend
-    const response = await fetch(`${context?.baseUrl}/graphql/auth`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        query: `
+  useEffect(() => {
+    // Get user details if the user is authenticated from the backend
+    const getUserDetails = async () => {
+      // Perform the fetch request using GraphQL in order to get the user details on the main app page
+      // Note: Please convert your id to an objectId in the backend
+      const response = await fetch(`${context?.baseUrl}/graphql/auth`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          query: `
             query PostUserDetailsResponse($_id : String!, $token : String!){
               PostUserDetailsResponse(_id : $_id, token : $token){
                 user {
@@ -62,31 +80,56 @@ const useUserDetails = () => {
                 }
               }
             `,
-        variables: {
-          _id: context?.userId ?? "",
-          token: context?.token ?? "",
-        },
-      }),
-    });
+          variables: {
+            _id: context?.userId ?? "",
+            token: context?.token ?? "",
+          },
+        }),
+      });
 
-    // Handle the response
-    const { data } = await response.json();
-    return data.PostUserDetailsResponse;
-  };
-
-  useEffect(() => {
+      // Handle the response
+      const { data } = await response.json();
+      return data.PostUserDetailsResponse;
+    };
     const handleRequest = async () => {
       try {
         // Get our values from local storage
         context?.validateAuthentication();
 
+        // Get the details from the backend
         if (context?.userAuthenticated && context.userId) {
           const { user, success, sessionCreated, sessionExpires } = await getUserDetails();
+
+          if (!success) {
+            setUserDetails((previousState) => {
+              return { ...previousState, error: true };
+            });
+            return;
+          }
+
+          setUserDetails((previousState) => {
+            return { ...previousState, user, sessionCreated, sessionExpires };
+          });
+
+          if (context?.userAuthenticated && context.userId && context.token) {
+            await checkSessionValidation(context.userId, context.token, context.baseUrl);
+          }
         }
       } catch {
-        
+        setUserDetails((previousState) => {
+          return { ...previousState, error: true };
+        });
       } finally {
+        setUserDetails((previousState) => {
+          return { ...previousState, isLoading: false };
+        });
       }
     };
+
+    handleRequest();
   }, [context]);
+
+  return userDetails;
 };
+
+export default useUserDetails;
