d46569b1d2f6b32575934ba907f183b7b1e0e1dc
Nothile Moyo <Nothile1@gmail.com>
2025-12-23 23:33:38 +0000

    We now mock the checkAuthentication method to increase the coverage
    Covearge is now at 61%. We still need a way of checking values, not just running the code

--- src/context/AppContext.test.tsx
+++ src/context/AppContext.test.tsx
@@ -6,13 +6,29 @@
  * Description: Test for the appContext
  */
 
-import { useContext } from "react";
-import { AppContext, ContextProps } from "./AppContext";
+import { act, useContext } from "react";
+import AppContextProvider, { AppContext, ContextProps } from "./AppContext";
 import { render } from "@testing-library/react";
 import { mockContext } from "../test-utils/mocks/objects";
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { clearAuthStorage, setMockAuthStorage } from "../test-utils/authStorage";
+
+beforeEach(() => {
+  setMockAuthStorage();
+});
+
+afterEach(() => {
+  clearAuthStorage();
+  jest.clearAllMocks();
+});
 
 describe("AppContext", () => {
+  /* const checkAuthenticationMock = jest.fn(() => true);
+  const failedCheckAuthenticationMock = jest.fn(() => false);
+  const validateAuthenticationMock = jest.fn(() => {});
+  const logoutUserMock = jest.fn(() => {}); */
+
   // Instantiate our context and create a child to pass it through, that way, we can test everything
   const Children = () => {
     const appContextInstance: ContextProps = useContext(AppContext);
@@ -20,7 +36,9 @@ describe("AppContext", () => {
     return (
       <section>
         <div>
-          <p>Token: {appContextInstance.token}</p>
+          <p data-testid="test-id-token-value">Token: {appContextInstance.token}</p>
+          <p data-testid="test-id-userid-value">UserId: {appContextInstance.userId}</p>
+          <p data-testid="test-id-authentication-value">Authenticated: {appContextInstance.userAuthenticated}</p>
         </div>
         <div>
           <button data-testid="test-id-validate-button" onClick={appContextInstance.validateAuthentication}>
@@ -43,8 +61,8 @@ describe("AppContext", () => {
 
     // Check to make sure we see the buttons
     const validateButton = screen.getByTestId("test-id-validate-button");
-    const checkAuthenticationButton = screen.getByTestId("test-id-validate-button");
-    const logoutButton = screen.getByTestId("test-id-validate-button");
+    const checkAuthenticationButton = screen.getByTestId("test-id-check-button");
+    const logoutButton = screen.getByTestId("test-id-logout-button");
     expect(validateButton).toBeVisible();
     expect(checkAuthenticationButton).toBeVisible();
     expect(logoutButton).toBeVisible();
@@ -52,4 +70,31 @@ describe("AppContext", () => {
     // Check if it matches the snapshot
     expect(baseElement).toMatchSnapshot();
   });
+
+  it("Calls the checkAuthentication method passed through", async () => {
+    // Render an empty component
+    const { baseElement } = render(<AppContextProvider>{<Children />}</AppContextProvider>);
+
+    // Check to make sure we see the buttons
+    const checkAuthenticationButton = screen.getByTestId("test-id-check-button");
+
+    // Click on the checkAuthentication button
+    userEvent.click(checkAuthenticationButton);
+
+    // Check if it matches the snapshot
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  /* it("Calls validateAuthentication with an environment of dev", () => {
+    // Render an empty component
+    const { baseElement } = render(<AppContextProvider>{<Children />}</AppContextProvider>);
+
+    // Check to make sure we see the buttons
+    const validateButton = screen.getByTestId("test-id-validate-button");
+
+    userEvent.click(checkAuthenticationButton);
+
+    // Check if it matches the snapshot
+    expect(baseElement).toMatchSnapshot();
+  }); */
 });
