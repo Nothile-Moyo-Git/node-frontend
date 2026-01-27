c18fd5b3cc57bb305d77532d315f4fad915da154
Nothile Moyo <Nothile1@gmail.com>
2026-01-27 23:42:06 +0000

    Added a few more bits for validateFields

--- src/pages/Posts/helpers/PostHelpers.ts
+++ src/pages/Posts/helpers/PostHelpers.ts
@@ -6,3 +6,42 @@
  *
  * Description: A helper file for Dealing with post validation
  */
+
+interface InputValuesProps {
+  title: string;
+  content: string;
+}
+
+type InputValidity {
+  formValid: 
+};
+
+const validateFields: InputValuesProps = ({ title, content }) => {
+  let inputsValid = true;
+
+  if (title.length < 3 || title.length > 100) {
+    setIsFormValid(false);
+    setIsTitleValid(false);
+    inputsValid = false;
+  } else {
+    setIsTitleValid(true);
+  }
+
+  if (content.length < 6 || content.length > 600) {
+    setIsFormValid(false);
+    setIsContentValid(false);
+    inputsValid = false;
+  } else {
+    setIsContentValid(true);
+  }
+
+  if (isDevelopment) {
+    if (!uploadFile) {
+      setIsFormValid(false);
+      setIsFileValid(false);
+      inputsValid = false;
+    }
+  }
+
+  return inputsValid;
+};
