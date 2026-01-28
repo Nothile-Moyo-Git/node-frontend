c900e7751e202fc457bf8936bef079c200ff367f
Nothile Moyo <Nothile1@gmail.com>
2026-01-28 23:17:45 +0000

    Made progress with the post helper, the goal is to make this function reusable for the forms that we have
    Mostly because EditPost is far too complex for an app of this size. So, we need to make it make more sense

--- src/pages/Posts/helpers/PostHelpers.ts
+++ src/pages/Posts/helpers/PostHelpers.ts
@@ -7,32 +7,33 @@
  * Description: A helper file for Dealing with post validation
  */
 
-interface InputValuesProps {
-  title: string;
-  content: string;
-}
-
-type InputValidity {
-  formValid: 
+type FormFieldItem = {
+  name: string;
+  value: string;
+};
+type FormFieldItems = {
+  fields: FormFieldItem[];
 };
 
-const validateFields: InputValuesProps = ({ title, content }) => {
-  let inputsValid = true;
+interface ValidateFieldsProps {
+  form: FormFieldItems;
+  isDevelopment: boolean;
+}
+
+const validateFields = ({ form, isDevelopment }: ValidateFieldsProps) => {
+  const title = form.fields.filter((field) => field.name === "title");
+  const content = form.fields.filter((field) => field.name === "content");
+  const image = form.fields.filter((field) => field.name === "image");
+
+  let titleValid,
+    contentValid = true;
 
   if (title.length < 3 || title.length > 100) {
-    setIsFormValid(false);
-    setIsTitleValid(false);
-    inputsValid = false;
-  } else {
-    setIsTitleValid(true);
+    titleValid = false;
   }
 
   if (content.length < 6 || content.length > 600) {
-    setIsFormValid(false);
-    setIsContentValid(false);
-    inputsValid = false;
-  } else {
-    setIsContentValid(true);
+    contentValid = false;
   }
 
   if (isDevelopment) {
